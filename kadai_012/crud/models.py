2657c5a7b252a44296d88b1b410ea1556fb16a1e
okisaku <nyotarisasianataka@outlook.jp>
2024-10-19 15:46:09 +0900

    Add files via upload

--- kadai_012/crud/models.py
+++ kadai_012/crud/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)  # 商品説明フィールド
- 
 class Category(models.Model):
     name = models.CharField(max_length=200)
  
@@ -17,10 +12,11 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     img = models.ImageField(blank=True, default='noImage.png')
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
+
     # 新規作成・編集完了時のリダイレクト先
     def get_absolute_url(self):
         return reverse('list')
